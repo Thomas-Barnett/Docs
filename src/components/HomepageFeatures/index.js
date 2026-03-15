db3337da189bf145492439c3335f646e07d82e2e
Thomas Barnett <tombarn65@gmail.com>
2026-03-14 21:34:28 -0400

    Update GitHub Actions versions

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -1,6 +1,7 @@
 import clsx from 'clsx';
 import Heading from '@theme/Heading';
 import styles from './styles.module.css';
+import useBaseUrl from '@docusaurus/useBaseUrl';
 
 const FeatureList = [
   /*{
@@ -25,7 +26,7 @@ const FeatureList = [
   },*/  
   {
     title: 'About Me',
-    ImgSrc: require('/home/hivemind/Projects/Portfolio-Site/tom-docs/static/img/about-me.jpg').default,
+    ImgSrc: require('@site/static/img/about-me.jpg').default,
     description: (
       <>
       	<p>
